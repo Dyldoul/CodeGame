c2958b49375521ebd6bc556843d8084be0f582bf
Dylan Munier <dylan.munier68@gmail.com>
2020-07-10 10:58:09 +0200

    -Ajout servlet Parametres -Ajout vue parametres.jsp -Ecriture fichier web.xml

---
+++ WebContent/WEB-INF/parametres.jsp
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+	
+		<meta charset="UTF-8">
+		<title>Options</title>
+		
+	</head>
+	
+	<body>
+	
+		<p><c:out value="La longueur du code secret est de : ${ options.longueurCode }"/></p>
+		
+		<p><c:out value="Le nombre d'éssais est fixé à : ${ options.nombreEssai }"/></p>
+		
+	
+	</body>
+
+</html>
---
+++ src/servlets/Parametres.java
@@ -0,0 +1,40 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Options;
+
+
+@WebServlet("/Parametres")
+public class Parametres extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public Parametres() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Options options = new Options();
+		request.setAttribute("options", options);
+		
+		this.getServletContext().getRequestDispatcher("/WEB-INF/parametres.jsp").forward(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		
+		
+	}
+
+}
